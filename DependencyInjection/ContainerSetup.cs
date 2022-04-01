03b23674bfd90dd508d30867c1a0f66afea40e9b
richard.smith <richard.smith@vianetplc.com>
2022-04-01 08:33:13 +0100

    Add project files.

---
+++ DependencyInjection/ContainerSetup.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using MineField.Common;
+using MineField.Grid.DepenencyInjection;
+
+namespace MineField
+{
+    public static class ContainerSetup
+    {
+        public static IContainer Container { get; set; }
+
+        public static void BuildContainer()
+        {
+            var container = new ContainerBuilder();
+
+            container.RegisterModule<GridModule>();
+
+            Container = container.Build();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using MineField.Common;
+using MineField.Common.Enumerations;
+using MineField.Logic;
+using System;
+
+namespace MineField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContainerSetup.BuildContainer();
+            InitialiseGame();            
+        }
+
+        static void InitialiseGame()
+        {
+            Console.WriteLine(UserMessages.Welcome);
+
+            var gameManager = ContainerSetup.Container.Resolve<IGameManager>();
+            var gameData = new GameData();
+
+            if (gameManager == null) throw new ArgumentNullException("Check the container is initialised correctly.");
+
+            gameManager.StartGame(gameData);
+
+            while (gameData.GameState == GameState.InProgress)
+            {
+                var message = gameManager.ProgressGame(gameData);
+
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
